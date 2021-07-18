bccca953f960324e013da72e38303678513ed780
Zackory Kelly <zackkelly@live.ca>
2021-07-18 03:48:29 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Zack Kelly</h1>
-        <p>Coming Soon</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="App-nav">
+          <Link className="nav-logo" to="/">
+            Zack Kelly
+          </Link>
+          <Link className="nav-link" to="/portfolio">
+            Portfolio
+          </Link>
+          <Link className="nav-link" to="/blog">
+            Blog
+          </Link>
+          <Link className="nav-link" to="/contact">
+            Contact Me
+          </Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <header className="App-header">
+              <h1>Zack Kelly</h1>
+              <p>Coming Soon</p>
+            </header>
+          </Route>
+          <Route path="/portfolio" exact>
+            <header className="App-header">
+              <h1>Portfolio</h1>
+              <p>Coming Soon</p>
+            </header>
+          </Route>
+          <Route path="/blog" exact>
+            <header className="App-header">
+              <h1>Blog</h1>
+              <p>Coming Soon</p>
+            </header>
+          </Route>
+          <Route path="/contact" exact>
+            <h1>Contact Me</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
